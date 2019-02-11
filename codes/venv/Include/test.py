8b1439b328c0fa5aab451694321477c1f75aedb8
FWWorks <214731923@qq.com>
2019-02-11 13:05:23 -0600

    update

---
+++ codes/venv/Include/test.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/dataview')
+def data_view():
+    return render_template('test.html')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
